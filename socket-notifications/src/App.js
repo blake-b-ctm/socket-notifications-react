0051f707ea160111bf6e2678de0f82238e0eac4f
Blake Burns <blake.burns@comparethemarket.com>
2023-04-26 12:38:48 +0100

    add first socket connection

--- socket-notifications/src/App.js
+++ socket-notifications/src/App.js
@@ -1,7 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import socket from "./socket";
 import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const sessionID = localStorage.getItem("sessionID");
+  const username = crypto.getRandomValues(new Uint32Array(1)).toString("hex");
+
+  const [showSession, setShowSession] = useState(false);
+
+  useEffect(() => {
+
+    if (sessionID) {
+      socket.auth = { sessionID };
+    } else {
+      socket.auth = { username };
+    }
+
+    socket.connect();
+
+    socket.on("session", ({ sessionID, userID }) => {
+      // attach the session ID to the next reconnection attempts
+      socket.auth = { sessionID };
+
+      // store it in the localStorage
+      localStorage.setItem("sessionID", sessionID);
+
+      // save the ID of the user
+      socket.userID = userID;
+
+      setShowSession(true);
+    });
+
+    socket.on("connect_error", (err) => {
+      console.log("connect error: ", err);
+    });
+    return function cleanup() {
+      socket.off("connect_error");
+    };
+  });
   return (
     <div className="App">
       <header className="App-header">
@@ -14,6 +52,11 @@ function App() {
         >
           Learn React
         </a>
+        {showSession &&
+          <h3>
+            You are connected to the notification socket service!
+          </h3>
+        }
       </header>
     </div>
   );
